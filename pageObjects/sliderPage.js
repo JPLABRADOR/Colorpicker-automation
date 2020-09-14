b1dbb00513c7a60f6c9f664e258eb15eba6c6c01
Jeremy Labrador <jeremylabrador@icloud.com>
2020-09-14 16:33:17 -0700

    Colorpicker Automation Page Object

--- pageObjects/sliderPage.js
+++ pageObjects/sliderPage.js
@@ -1,25 +1,25 @@
 var sliderPage = function () {
-	
-	this.setApp = function(app) {
-		this.app = app
-	}
-	
-	this.setTitle = function(title) {
-		this.tile = title
-	}	
 
-	this.verifySliderFunctionality = function(testData) {
-		if (this.tile) {
-			console.log(testData.testName)
-			return this.app.client
-				.setValue(testData.redVauleSelector, testData.redValue)
-				.setValue(testData.greenValueSelector, testData.greenValue)
-				.setValue(testData.blueValueSelector, testData.blueValue)
-		} else {
-			const message = 'ERROR: Verifying Window Title'
-			throw new Error(message)
-		}
-	}
+    this.setApp = function(app) {
+        this.app = app
+    }
+
+    this.setTitle = function(title) {
+        this.tile = title
+    }	
+
+    this.verifySliderFunctionality = function(testData) {
+        if (this.tile) {
+            console.log(testData.testName)
+            return this.app.client
+                .setValue(testData.redVauleSelector, testData.redValue)
+                .setValue(testData.greenValueSelector, testData.greenValue)
+                .setValue(testData.blueValueSelector, testData.blueValue)
+        } else {
+            const message = 'ERROR: Verifying Window Title'
+            throw new Error(message)
+        }
+    }
 }
 
 module.exports = sliderPage;
