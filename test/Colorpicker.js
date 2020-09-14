d0662f998bd0a55be3b6110c0f2e1d6b33d05a0f
Jeremy Labrador <jeremylabrador@icloud.com>
2020-09-14 16:30:47 -0700

    Colorpicker Automation Test File

--- test/Colorpicker.js
+++ test/Colorpicker.js
@@ -7,55 +7,55 @@ var titleExists
 var expectedTitle = 'Colorpicker'
 
 describe('Colorpicker Slider Tests', function () {
-	this.timeout(10000)
+    this.timeout(10000)
 
-	beforeEach(function () {
-		this.app = new Application({
-			path: 'C:\\Program Files\\Colorpicker\\Colorpicker.exe'
-		})
-		page = new slider()
-		page.setApp(this.app)
-		page.setTitle(titleExists)
-		return this.app.start()
-	})
+    beforeEach(function () {
+        this.app = new Application({
+            path: 'C:\\Program Files\\Colorpicker\\Colorpicker.exe'
+        })
+        page = new slider()
+        page.setApp(this.app)
+        page.setTitle(titleExists)
+        return this.app.start()
+    })
 
-	afterEach(function () {
-		if (this.app && this.app.isRunning()) {
-			return this.app.stop()
-		}
-	})
-	
+    afterEach(function () {
+        if (this.app && this.app.isRunning()) {
+            return this.app.stop()
+        }
+    })
+
     it('The Window Title Was Found', function () {
-		this.app.client.title().then(function (actual) {
-			titleExists = expectedTitle == actual.value
-			assert.strictEqual(actual, expectedTitle)
-		})
-	})	
+        this.app.client.title().then(function (actual) {
+            titleExists = expectedTitle == actual.value
+            assert.strictEqual(actual, expectedTitle)
+        })
+    })	
 
-	sliderData.forEach(function (test) { 
-		it('Slider Should Function Correctly', function () {
-			return page
-				.verifySliderFunctionality(test)
-				.getAttribute(test.redSliderValueSelector, test.sliderValueAttribute)
-				.then(function (actual) {
-					assert.strictEqual(actual, test.redSliderValue)
-				})
-				.getAttribute(test.greenSliderValueSelector, test.sliderValueAttribute)
-				.then(function (actual) {
-					assert.strictEqual(actual, test.greenSliderValue)
-				})
-				.getAttribute(test.blueSliderValueSelector, test.sliderValueAttribute)
-				.then(function (actual) {
-					assert.strictEqual(actual, test.blueSliderValue)
-				})			
-				.getAttribute(test.rgbValueSelector, test.rgbValueAttribute)
-				.then(function (actual) {
-					assert.strictEqual(actual, test.rgbValue)
-				})
-				.getValue(test.hexValueSelector)
-				.then(function (actual) {
-					assert.strictEqual(actual, test.hexValue)
-				})
-		})
-	})
+    sliderData.forEach(function (test) { 
+        it('Slider Should Function Correctly', function () {
+            return page
+                .verifySliderFunctionality(test)
+                .getAttribute(test.redSliderValueSelector, test.sliderValueAttribute)
+                .then(function (actual) {
+                    assert.strictEqual(actual, test.redSliderValue)
+                })
+                .getAttribute(test.greenSliderValueSelector, test.sliderValueAttribute)
+                .then(function (actual) {
+                    assert.strictEqual(actual, test.greenSliderValue)
+                })
+                .getAttribute(test.blueSliderValueSelector, test.sliderValueAttribute)
+                .then(function (actual) {
+                    assert.strictEqual(actual, test.blueSliderValue)
+                })			
+                .getAttribute(test.rgbValueSelector, test.rgbValueAttribute)
+                .then(function (actual) {
+                    assert.strictEqual(actual, test.rgbValue)
+                })
+                .getValue(test.hexValueSelector)
+                .then(function (actual) {
+                    assert.strictEqual(actual, test.hexValue)
+                })
+        })
+    })
 })
